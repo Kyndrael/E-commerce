df7812d5551603d93ea5404c38306409348857af
Kyndrael <jonathansilveira95@hotmail.com>
2025-05-18 11:49:34 -0300

    Create 02 inserção dados.sql

---
+++ script/02 inserção dados.sql
@@ -0,0 +1,5 @@
+-- Dados fictícios de clientes
+INSERT INTO Cliente VALUES (1, 'PF', 'João Silva', 'joao@email.com', '11999999999');
+INSERT INTO ClientePF VALUES (1, '12345678900');
+
+-- ... (demais inserts, conforme enviado antes)
